48b090740ce3a570bf5298b945fd73975f1817c4
jyHyun <zex199@naver.com>
2023-12-18 10:57:10 +0900

    Admin superuser 추가, Board 모델 추가

--- newspeace/board/models.py
+++ newspeace/board/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+class Board(models.Model):   # 테이블 명은 '앱이름_모델명소문자' 로 만들어짐. 
+    title = models.CharField(max_length=250)            # 제목
+    body = models.TextField()                           # 본문
+    image= models.ImageField(blank=True, upload_to='board/') # 이미지
+    created = models.DateField(auto_now_add=True)   # 작성한 날짜    
+    
+    def __str__(self):  # admin이나 shell에서 모델 인스턴스 출력이 어떻게 보일지
+        return self.title
