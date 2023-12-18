8f53150d202e4bf31cffbea157a5f1757398a274
jyHyun <zex199@naver.com>
2023-12-18 11:19:52 +0900

    공지사항 목록보기 구현완료

--- newspeace/board/urls.py
+++ newspeace/board/urls.py
@@ -4,6 +4,6 @@ from .import views
 app_name = 'board'
 urlpatterns = [
     # 유지보수를 위해 name 속성으로 URL에 이름을 지정해줌
-    path('', views.test1, name='test1'),                  # http://localhost:8000/board/
-    path('test1/',views.test1),
+    path('', views.list, name='list'),
+    path('write/', views.write, name='write'),
 ]
--- newspeace/board/views.py
+++ newspeace/board/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import *
 
 def test1(request):
     return HttpResponse('board/test1 응답!')
+
+# Board 전체 목록 보기
+def list(request):
+    board_list = Board.objects.all()
+    return render(request, 'board/list.html', {'board_all':board_list}) # HttpRequest()로 list.html 템플릿 페이지를 Response의 body에 응답
+
+# # Form 기반으로 Board Data 추가 작업
+def write(request):
+    return render(request, 'board/write.html')
