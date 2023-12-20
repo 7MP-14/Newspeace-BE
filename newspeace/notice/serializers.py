93df75473783d4e9b95a3dd943901f2603b7facc
jyHyun <zex199@naver.com>
2023-12-20 16:37:37 +0900

    공지사항 게시판 API 수정

--- newspeace/notice/serializers.py
+++ newspeace/notice/serializers.py
@@ -3,16 +3,15 @@ from board.models import Board
 
 
 class NoticeListSerializer(serializers.ModelSerializer):
-    # category = serializers.CharField(source='category.name')
-    class Meta:
-        model = Board
-        fields = ['id', 'title', 'created', 'author']
-        
-        
-class NoticeRetrieveSerializer(serializers.ModelSerializer):
     class Meta:
         model = Board
         fields = '__all__'
+        
+        
+# class NoticeRetrieveSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = Board
+#         fields = '__all__'
         
         
 class NoticeDestroySerializer(serializers.ModelSerializer):
--- newspeace/notice/views.py
+++ newspeace/notice/views.py
@@ -4,28 +4,37 @@ from rest_framework.generics import *
 from board.models import Board
 
 
+# 공지사항 게시판 목록, 게시글 세부
 class NoticeListAPIView(ListAPIView):
     queryset = Board.objects.all()
     serializer_class = NoticeListSerializer
     
-    
-class NoticeRetrieveAPIView(RetrieveAPIView):
-    queryset = Board.objects.all()
-    serializer_class = NoticeRetrieveSerializer
-    
-    def get_serializer_context(self):
-        return {
-            'request' : None,
-            'format' : self.format_kwarg,
-            'view' : self
-        }
+    # def get_serializer_context(self):
+    #     return {
+    #         'request' : None,
+    #         'format' : self.format_kwarg,
+    #         'view' : self
+    #     }
     
     
+# class NoticeRetrieveAPIView(RetrieveAPIView):
+#     queryset = Board.objects.all()
+#     serializer_class = NoticeRetrieveSerializer
+    
+    
+# 게시글 추가
 class NoticeCreateAPIView(CreateAPIView):
     queryset = Board.objects.all()
-    serializer_class = NoticeRetrieveSerializer
+    serializer_class = NoticeListSerializer
+    
+    def create(self, request, *args, **kwargs):
+        response = super().create(request, *args, **kwargs)
+        response["Access-Control-Allow-Origin"] = "http://localhost:3000"
+        response["Access-Control-Allow-Credentials"] = "true"
+        return response
     
     
+# 게시글 삭제
 class NoticeDestroyAPIView(DestroyAPIView):
     queryset = Board.objects.all()
     serializer_class = NoticeDestroySerializer
