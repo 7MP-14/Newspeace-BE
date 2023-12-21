e8cdff8ba6133ee20ec9665c30f9d252f2c5b056
nchj <dltnstlssnr1@naver.com>
2023-12-21 16:06:33 +0900

    mynews_스크립트 추가

--- newspeace/news/urls.py
+++ newspeace/news/urls.py
@@ -4,4 +4,5 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('search/', views.search, name="news-search"),
+    path('mynews/', views.newsScript, name="news-script"),
 ]
