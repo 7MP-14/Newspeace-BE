4197ea09f0b11afd0946149a1b92d67608e624c5
nchj <dltnstlssnr1@naver.com>
2023-12-29 16:57:02 +0900

    뷰함수import파일추가

---
+++ newspeace/mykeyword_graph_play.py
@@ -0,0 +1,9 @@
+from mykeyword_graph import mykeyword_negative_update
+
+print("start")
+
+crwaling_day = 29
+crwaling_hour = 14
+
+response = mykeyword_negative_update(crwaling_day, crwaling_hour)
+print(response)
